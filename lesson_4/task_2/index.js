416cd9a5fa078121b490b713adcb33da6183bbb2
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-04-15 15:30:06 +0300

    JS_4_2

--- lesson_4/task_2/index.js
+++ lesson_4/task_2/index.js
@@ -1,11 +1,10 @@
-const m = 1;
+const m = 3;
 const n = 10;
 let result = 1;
 
-for (let i = 1; i <= n ; i++) {
-    if (m % 2 === 1) {
+for (let i = m; i <= n; i++) {
+  if (i % 2 === 1) {
     result *= i;
-    }
+  }
 }
-
 console.log("Result: ", result);
