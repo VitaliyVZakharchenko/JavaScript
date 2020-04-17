1acaa5c7c8cbd0f70164987068cf1e45b241f2b0
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-04-17 14:40:16 +0300

    JS_5_hw_2

--- lesson_5/hw_2/index.js
+++ lesson_5/hw_2/index.js
@@ -1,13 +1,10 @@
 "use strict";
 
 const getPrimes = function (n) {
-  let count = 2;
-  for (let i = count; i <= n; i++) {
-    for (let j = 1; j <= i; j++) {
+    for (let i = 2; i <= n; i++) {
+    for (let j = 2; j < i; j++) {
       if (i % j === 0) continue;
     }
     console.log(i);
 }
 }
-
-// getPrimes(30);
