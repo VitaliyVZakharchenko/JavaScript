6117ed287258b396c1ea4f734e07c80e7678b513
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-04-19 13:12:08 +0300

    JS_6_task_3

--- lesson_6/task_3/index.js
+++ lesson_6/task_3/index.js
@@ -4,7 +4,7 @@ const getSpecialNumbers = function (m, n) {
     let result = [];
 
     for (let i = m; m <= n; i++) {
-        if (m % 3 === 0) {
+        if (i % 3 === 0) {
             result.push(i);
         }
     }
