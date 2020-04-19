725836300d38d70ee1da16c61f43bccf01a0ef37
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-04-19 13:00:18 +0300

    JS_6_task_2

---
+++ lesson_6/task_2/index.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const getSum = function (arr) {
+    let sum = 0;
+    if (!Array.isArray(arr)) {
+        return null;
+    }
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
