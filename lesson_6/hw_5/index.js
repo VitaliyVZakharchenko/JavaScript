455085ef9d0fa97451d46f52c4087f01edb83bb4
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-04-20 14:55:35 +0300

    JS_6_hw_5

---
+++ lesson_6/hw_5/index.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const reverseArray = function (arr) {
+    let result = [];
+
+    if (!Array.isArray(arr)) {
+        return null;
+      }
+
+    for (let i = 1; i <= arr.length; i++) {
+        result.push(arr[arr.length - i]);
+    }
+    return result;
+}
+
+console.log(reverseArray([1, 2, 3, 4, 5]));
