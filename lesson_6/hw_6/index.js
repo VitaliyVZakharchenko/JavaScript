7537b9478bbac3a0f1ff4f78a91a700b366118d8
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-04-20 11:08:48 +0300

    JS_6_hw_6

--- lesson_6/hw_6/index.js
+++ lesson_6/hw_6/index.js
@@ -1,16 +1,12 @@
-'use strict'
+"use strict";
 
 const cloneArr = function (arr) {
-    let newArr = [];
-    if (!Array.isArray(arr)) {
-        return null;
-    } else {
-    newArr = arr.slice();
-    // return;
-    console.log(newArr);
-    }
-    
-    return;
-}
+  let newArr = [];
 
-console.log(cloneArr([1, 2, 3, 4, 5]));
+  if (!Array.isArray(arr)) {
+    return null;
+  } else {
+    newArr = arr.slice();
+  }
+  return newArr;
+}
