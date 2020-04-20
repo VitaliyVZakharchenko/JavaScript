4678a3eaea87d700930e42f4cf803a28dab1c875
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-04-20 13:05:23 +0300

    JS_6_hw_4

---
+++ lesson_6/hw_4/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const increaseEvenEl = function (arr, delta) {
+
+  if (!Array.isArray(arr)) {
+    return null;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+        arr[i] + delta;
+      }
+    }
+}
