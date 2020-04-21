5a755992341d96f15cdc1157cf1e49d844a4b847
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-04-21 21:53:49 +0300

    lesson_7_hw_6

--- lesson_7/hw_6/index.js
+++ lesson_7/hw_6/index.js
@@ -2,11 +2,9 @@
 
 const sum = arr =>
     !Array.isArray(arr) ? null :
-    arr.reduce((acc, elem) => {
-        return acc + elem;
-});
+    arr.reduce((acc, elem) => acc + elem);
 
 
-const numbers = [1, 2, 3]
+const numbers = [1, 2, 3, 1]
 
 console.log(sum(numbers));
