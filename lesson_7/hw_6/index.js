365788189f0aaffb09416b254c75d6b1333a01ef
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-04-21 21:51:00 +0300

    lesson_7_hw_6

---
+++ lesson_7/hw_6/index.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const getSum = arr =>
+    !Array.isArray(arr) ? null :
+    arr.reduce((acc, elem) => {
+        return acc + elem;
+});
+
+
+const numbers = [1, 2, 3]
+
+console.log(getSum(numbers));
