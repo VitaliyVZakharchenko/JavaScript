e121bf354603cd68070377e0795ea8c862174361
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-04-21 16:59:04 +0300

    lesson_7_task_1-4

--- lesson_7/task_2/index.js
+++ lesson_7/task_2/index.js
@@ -1,17 +1,4 @@
 'use strict'
 
-const getSpecialNumbers = numbers =>
-    numbers.filter(num => num % 3 === 0);
-
-const sortArray = numbers =>
+const sortDesc = numbers =>
 numbers.slice().sort((a, b) => b - a);
-
-const flatArray = arr => arr
-.reduse((acc, elem) => {
-    return acc.concat(elem);
-}, []);
-
-const getMessageForPassedStudents = (allStudents, failedStudents) =>
-    allStudents
-        .filter(name => !failedStudents.includes(name))
-        .map(name => 'Good job, ' + name);
--- lesson_7/task_4/index.js
+++ lesson_7/task_4/index.js
@@ -1,17 +1,6 @@
 'use strict'
 
-const getSpecialNumbers = numbers =>
-    numbers.filter(num => num % 3 === 0);
-
-const sortArray = numbers =>
-numbers.slice().sort((a, b) => b - a);
-
-const flatArray = arr => arr
-.reduse((acc, elem) => {
-    return acc.concat(elem);
-}, []);
-
-const getMessageForPassedStudents = (allStudents, failedStudents) =>
+const getMessagesForBestStudents = (allStudents, failedStudents) =>
     allStudents
         .filter(name => !failedStudents.includes(name))
         .map(name => 'Good job, ' + name);
