75eb7b7dad79190f7cb9cc9a54e5a62233be0cc7
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-04-21 13:50:22 +0300

    lesson_6_hw_8

---
+++ lesson_6/hw_8/index.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const sortAsc = function (array) {
+    for (let i = 0; i < array.length; i += 1) {
+        for (let j = 0; j < array.length-1-i; j += 1) {
+            if (array[j+1] < array[j]) {
+                let t = array[j+1]; array[j+1] = array[j]; array[j] = t;
+            }
+        }
+     }                     
+    return array;
+}
+
+const sortDesc = function (array) {
+    for (let i = 0; i < array.length; i += 1) {
+        for (let j = 0; j < array.length-1-i; j += 1) {
+            if (array[j+1] > array[j]) {
+                let t = array[j+1]; array[j+1] = array[j]; array[j] = t;
+            }
+        }
+     }                     
+    return array;
+}
+
+const numbers = [9, 5, 10, 4, 7, 67, 3];
+const result = sortDesc(numbers);
+
+console.log(result);
