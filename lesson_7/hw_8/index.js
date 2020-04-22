5731b67640959f84a53e9e512d240f99896b9af8
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-04-22 12:52:11 +0300

    lesson_7_hw_8

--- lesson_7/hw_8/index.js
+++ lesson_7/hw_8/index.js
@@ -1,9 +1,9 @@
 'use strict'
 
-const arrAverege = (arr) =>
+const arrAverage = (arr) =>
     !Array.isArray(arr) ? null :
     arr.reduce((accumulator, currentValue) =>
     accumulator + currentValue) / arr.length;
 
 const arr = [2, 5, 6, 3];
-console.log(arrAverege(arr));
+console.log(arrAverage(arr));
