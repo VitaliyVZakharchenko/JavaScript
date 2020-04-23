851e225315b7f4a833be23189031e575d6d19d7e
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-04-23 14:03:42 +0300

    lesson_8_hw_4

--- lesson_8/hw_4/index.js
+++ lesson_8/hw_4/index.js
@@ -4,20 +4,23 @@ const user = {
     name:'Sam'
 }
 
-const addPropertyV1 = (userData, userId) => userData.id = userId;
+const addPropertyV1 = (userData, userId) => {
+    userData.id = userId;
+    return userData;
+};
 
 const addPropertyV2 = (userData, userId) => {
     Object.assign(userData.id = userId);
     return userData;
 };
 
-const addPropertyV3 = (userData, userId) => 
-    Object.assign(userData, {id: userId});
+const addPropertyV3 = (userData, userId) =>
+    Object.assign({}, userData, {id: userId});
  
 const addPropertyV4 = (userData, userId) => {
     let newObj = {...userData, id: userId};
     return newObj;
 };
 
-const result = addPropertyV4(user, '12345');
+const result = addPropertyV3(user, '12345');
 console.log(result);
