f7ce7664584a5d49042828b7eabbc2c1c1f4f744
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-04-24 14:25:11 +0300

    lesson_8_hw_10

--- lesson_8/hw_10/index.js
+++ lesson_8/hw_10/index.js
@@ -1,19 +1,19 @@
-'use strict'
+"use strict";
 
 const objC = {
-    'John Doe': 19,
-    'Tom': 17,
-    'Bob': 18
+  "John Doe": 19,
+  Tom: 17,
+  Bob: 18,
 };
 
-const getAdults = (obj) => {
-    let result = {};
-    for (let prop in obj) {
-        if ( obj[prop] > 18 ) {
-            result[prop] = obj[prop];
-        }
+const getAdults = obj => {
+  let result = {};
+  for (let prop in obj) {
+    if (obj[prop] >= 18) {
+      result[prop] = obj[prop];
     }
-    return result;
+  }
+  return result;
 };
 
 const result = getAdults(objC);
