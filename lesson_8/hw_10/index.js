1636f8527a26f745ed3b111b3e54bfa7fd267deb
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-04-24 14:21:29 +0300

    lesson_8_hw_10

---
+++ lesson_8/hw_10/index.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const objC = {
+    'John Doe': 19,
+    'Tom': 17,
+    'Bob': 18
+};
+
+const getAdults = (obj) => {
+    let result = {};
+    for (let prop in obj) {
+        if ( obj[prop] >= 18 ) {
+            result[prop] = obj[prop];
+        }
+    return result;
+    }
+};
+
+const result = getAdults(objC);
+console.log(result);
