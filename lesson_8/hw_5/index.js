fbbbc3cdea8d5e79f82322d51d0a8caa819cf27e
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-04-27 14:15:44 +0300

    lesson_8_hw_5

--- lesson_8/hw_5/index.js
+++ lesson_8/hw_5/index.js
@@ -1,26 +1,31 @@
-// 'use strict'
+'use strict'
 
-// const user = {
-//     name:'Sam'
-// }
+const transaction = {
+    value: 170,
+}
 
-// const addPropertyV1 = (userData, userId) => {
-//     userData.id = userId;
-//     return userData;
-// };
+const addPropertyV1 = (obj, key, value) => {
+    obj[key] = value;
+    return obj;
+};
 
-// const addPropertyV2 = (userData, userId) => {
-//     Object.assign(userData.id = userId);
-//     return userData;
-// };
+const addPropertyV2 = (obj, key, value) => {
+    Object.assign(obj[key] = value);
+    return obj;
+};
 
-// const addPropertyV3 = (userData, userId) =>
-//     Object.assign({}, userData, {id: userId});
- 
-// const addPropertyV4 = (userData, userId) => {
-//     let newObj = {...userData, id: userId};
-//     return newObj;
-// };
 
-// const result = addPropertyV3(user, '12345');
-// console.log(result);
+const addPropertyV3 = (obj, key, value) => {
+    let newObj = {};
+
+    Object.assign(newObj, obj);
+    newObj[key] = value;
+    return newObj;
+};
+
+const addPropertyV4 = (obj, key, value) => 
+    ({...obj, [key]: value});
+    // result = {...obj, [key]: value}
+    // return result;
+    
+console.log(addPropertyV4(transaction, 'currency', 'USD'));
