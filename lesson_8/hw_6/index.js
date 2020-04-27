8c0cd722ee0eb493f9c372194e86b53f73a6e5ca
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-04-27 13:04:23 +0300

    lesson_8_hw_6

---
+++ lesson_8/hw_6/index.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const obj1 = {
+    name: 'Tom', 
+    age: 17
+};
+
+const obj2 = {
+    name: 'Bob',
+    student: false
+};
+
+const mergeObjectsV1 = (obj1, obj2) =>
+    Object.assign(obj1, obj2);
+
+const mergeObjectsV2 = (obj1, obj2) =>
+    Object.assign(obj2, obj1);
+
+const mergeObjectsV3 = (obj1, obj2) => {
+    return ({...obj1, ...obj2});
+};
+
+const mergeObjectsV4 = (obj1, obj2) => {
+    return {...obj2, ...obj1};
+};
+
+console.log(mergeObjectsV4(obj1, obj2));
