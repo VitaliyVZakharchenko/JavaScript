68b0722c7f1f2e9df9a5b5fe739494f6956a597b
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-04-27 16:51:53 +0300

    lesson_9_task_1

---
+++ lesson_9/task_1/index.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const users = {
+    'John Doe': 19,
+    'Tom': 17,
+    'Bob': 18,
+}
+
+const getAdults = usersObj => Object.entries(usersObj)
+    .filter(user => user[1] >= 18)
+    .map(user => user[0]);
+
+console.log(getAdults(users));
