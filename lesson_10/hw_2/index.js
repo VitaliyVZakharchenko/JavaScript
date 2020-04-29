56d0b0e4407a1f90195184ccb9d92112c218e8cb
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-04-29 17:02:11 +0300

    lesson_10_hw_2

--- lesson_10/hw_2/index.js
+++ lesson_10/hw_2/index.js
@@ -4,14 +4,18 @@ const getRandomNumbers = (leng, min, max) => {
     if ((max < min) || (max - min < 1) && Math.ceil(max) === Math.ceil(min)) {
         return null;
     };
-    
+
     let arr = [];
         
+    // for (let i = 0; i < leng; i += 1) {
+    //     arr[i] = Math.floor((Math.random() * (max - min) + 1) + min);
+    // }
+
     for (let i = 0; i < leng; i += 1) {
-        arr[i] = Math.floor((Math.random() * (max - min + 1)) + min);
-    }
+        arr[i] = Math.floor((Math.random() * (max + 1) + min));
+    };
 
     return arr;
 };
 
-console.log(getRandomNumbers(5, 1, 15));
+console.log(getRandomNumbers(5, 1, 20));
