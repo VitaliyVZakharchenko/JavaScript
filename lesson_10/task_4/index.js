83b479bb3ba109721ce6e395773a68332070102e
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-04-29 11:41:05 +0300

    lesson_10_task_4

--- lesson_10/task_4/index.js
+++ lesson_10/task_4/index.js
@@ -1,9 +1,5 @@
 'use strict'
 
-// const number = [-777, 3, 4.548, -5.24546, -1, 45, -20];
-// const number = [];
-// const number = '';
-
 const getMaxAbsoluteNumber = arr =>
     !Array.isArray(arr) || arr.length === 0 ? null :
     Math.max(...arr.map(currentValue =>
