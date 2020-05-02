6356edd59cee84cb605d6bcb2eb284f8c20a0dbc
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-05-02 14:09:01 +0300

    lesson_11_task_1

--- lesson_11/task_1/index.js
+++ lesson_11/task_1/index.js
@@ -5,18 +5,20 @@ const splitText = (text, len = 10) => {
         return null;
     }
 
-    let srtArr = [];
+    const strArr = [];
     let startPosition = 0;
-
+    
     while (true) {
-        let chunk = text.substr(startPosition, len);
+        let chunk = text.substr(startPosition, len); 
         if (chunk.length === 0) {
             break;
         }
-        srtArr.push(chunk[0].toUpperCase() + chunk.slice(1));
-        startPosition =+ len;
-    }
-    return srtArr.join('\n');
-};
 
-console.log(splitText('abcdefgh', 3));
+        strArr.push(chunk[0].toUpperCase() + chunk.slice(1));
+        startPosition += len;
+        
+    }
+    return strArr.join('\n');
+}
+
+console.log(splitText('abcdefgh', 2));
