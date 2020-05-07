a12ff878c8ae0b379053e0599f736c4cad782f27
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-05-07 15:54:48 +0300

    lesson_14_task_1

---
+++ lesson_14/task_1/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let message = "Just learn it";
+
+function sendMessage(name) {
+  const sender = "Gromcode";
+
+  console.log(`${name}, ${message}! Your ${sender}`);
+}
+
+function setMessage(text) {
+  message = text;
+}
