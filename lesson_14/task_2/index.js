51ef920d1ab57aefc218b7cf9e979d262a28b25a
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-05-08 12:49:29 +0300

    lesson_14_task_2

--- lesson_14/task_2/index.js
+++ lesson_14/task_2/index.js
@@ -1,9 +1,12 @@
+'use strict';
+    
 const createMesseger = () => {
     let sender = 'Gromcode';
-    let message = 'Just learn it!';
+    let message = 'Just learn it';
+    
     function sendMessage(name) {
 
-        console.log(`${name}, ${message} Your ${sender}`);
+        console.log(`${name}, ${message}! Your ${sender}`);
     }
 
     function setMessage(text) {
@@ -18,3 +21,18 @@ const createMesseger = () => {
         setSender
     };
 };
+
+export default createMesseger;
+
+const messenger = createMesseger();
+messenger.sendMessage('Bob');
+
+
+messenger.setMessage('Good job');
+messenger.sendMessage('Bob');
+
+
+const newMessage = 'Gromcode';
+messenger.sendMessage('Bob');
+
+messenger.setSender(newMessage.bold());
