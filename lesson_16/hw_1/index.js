bc26d6fd8445061208e350c6b42b29872ba5ce45
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-05-11 17:47:42 +0300

    lesson_16_hw_1

--- lesson_16/hw_1/index.js
+++ lesson_16/hw_1/index.js
@@ -3,7 +3,7 @@
 export const createArrayOfFunctions = (num) => {
   let arr = [];
 
-  !Number.isNaN(num) || typeof num === undefined ? null : arr;
+  (!Number.isNaN(num)) || (typeof num === undefined) ? null : arr;
 
   for (let i = 0; i < num; i++) {
     arr[i] = function () {
