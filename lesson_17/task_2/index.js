f3b350c5421a51630cfb615899a0a540d5b59013
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-05-12 16:43:07 +0300

    lesson_17_task_2

---
+++ lesson_17/task_2/index.js
@@ -0,0 +1,12 @@
+const callbackPromt = {
+    message: ' Tell me your nember',
+    showPromt() {
+        const phoneNumber = prompt(this.message);
+        console.log(phoneNumber);
+    },
+    showDeferredPromt(ms) {
+        setTimeout(this.showPromt.bind(this), ms);
+    }
+}
+
+callBackPromt.showDeferredPromt(1000);
