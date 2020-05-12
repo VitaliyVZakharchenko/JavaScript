939ca6d8f58c86df7f3a42e60893dd4c24bb256e
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-05-12 12:39:42 +0300

    lesson_16_hw_1

--- lesson_16/hw_1/index.js
+++ lesson_16/hw_1/index.js
@@ -3,7 +3,7 @@
 export const createArrayOfFunctions = (num) => {
   let arr = [];
   if (!Number.isInteger(num)) return null;
-  if (typeof(num) === "undefined") return arr;
+  if (typeof(num) === undefined) return arr;
 
   for (let i = 0; i < num; i++) {
     arr[i] = function () {
