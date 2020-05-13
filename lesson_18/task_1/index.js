c5d7b0157d19bd72b654764504632b179086882b
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-05-13 22:36:03 +0300

    lesson_18_task_1

---
+++ lesson_18/task_1/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const event = {
+    guests: [
+        { name: ' Tom', email: 't@gmail.com', age: 17 },
+        { name: ' Bob', email: 'b@gmail.com', age: 18 }
+    ],
+    message: 'Welcome to the party!',
+    getInvintations() {
+        return this.guests
+            .filter(({ age }) => age >= 18)
+            .map(({ name, email }) => ({
+                email,
+                text: `Dear ${name}! ${this.message}`,
+        }));
+    }
+}
+
+console.log(event.getInvintations());
