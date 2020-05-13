3214e7b935f409d028f01f42d5c980afb39d3199
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-05-13 22:41:44 +0300

    lesson_18_task_1

--- lesson_18/task_1/index.js
+++ lesson_18/task_1/index.js
@@ -6,7 +6,7 @@ export const event = {
         { name: 'Bob', email: 'b@gmail.com', age: 18 }
     ],
     message: 'Welcome to the party!',
-    getInvintations() {
+    getInvitations() {
         return this.guests
             .filter(({ age }) => age >= 18)
             .map(({ name, email }) => ({
@@ -16,4 +16,4 @@ export const event = {
     }
 };
 
-console.log(event.getInvintations());
+console.log(event.getInvitations());
