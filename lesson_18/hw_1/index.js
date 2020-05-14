dcd711c9996085c7b9f5a91f607e6e5cf98a0759
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-05-14 15:20:05 +0300

    lesson_18_hw_1

---
+++ lesson_18/hw_1/index.js
@@ -0,0 +1,11 @@
+export function saveCalls(func) {
+    const calls = [];
+
+    function withMemory(...arguments) {
+      calls.push(arguments);
+      return func.apply(this, arguments);
+    }
+
+    withMemory.calls = calls;
+    return withMemory;
+  }
