2ff9d13a8ecba9a477ca4708f007b5f033988ddb
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-05-15 14:33:40 +0300

    lesson_19_task_1

---
+++ lesson_19/task_1/index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+export const vehicle = {
+    name: 'Argo',
+    move() {
+        console.log(`${this.name} is moving`);
+    },
+    stop() {
+        console.log(`${this.name} stopped`);
+    }
+};
+
+export const ship = {
+    startMachine() {
+        console.log(`${this.name} lifting anchor up`);
+        this.move();
+    },
+    stopMachine() {
+        this.stop();
+        console.log(`${this.name} lifting anchor down`);
+    },
+    __proto__: vehicle
+};
+
+// Object.setPrototypeOf(ship, vehicle); 2й способ
+
+ship.startMachine();
+ship.stopMachine();
