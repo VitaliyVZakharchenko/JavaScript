207eff3b8e2ab0d3145bf14dadf3c250e9980db9
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-05-20 22:42:45 +0300

    lesson_20_task_1

---
+++ lesson_20/task_1/index.js
@@ -0,0 +1,30 @@
+export class User {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    static createEmpty() {
+        return new User('', null);
+      }
+
+    sayHi() {
+        console.log(`Hi, I am ${this.name}`);
+    }
+
+    requestNewPhoto() {
+        console.log(`New photo request was sent for ${this.name}`)
+    }
+
+    setAge(value) {
+        if (value < 0) {
+            return false;
+        }
+        this.age = value;
+
+        if (value >= 25) {
+            this.requestNewPhoto();
+        }
+        return value;
+    }
+}
