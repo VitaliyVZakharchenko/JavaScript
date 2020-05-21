d2ae074a2b0c2c17920734174f68f8b71784b1e2
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-05-21 14:41:17 +0300

    lesson_20_hw_1

---
+++ lesson_20/hw_1/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+class Order {
+    constructor(price, city, type) {
+        this.id = String(Math.random());
+        this.price = price;
+        this.dateCreated;
+        this.isConfirmed;
+        this.dateConfirmed;
+        this.city = city;
+        this.type = type;
+    }
+
+    checkPrice(price) {
+        // if (price > 1000) {
+        //     return true;
+        // } else {
+        //     return false;
+        // }
+        return price > 1000;
+    }
+
+    checkOrder() {
+        this.isConfirmed = true;
+        this.dateConfirmed = new Date();
+    }
+
+    isValidType() {
+        return (this.type === 'Buy' || this.type === 'Sell');
+    }
+}
