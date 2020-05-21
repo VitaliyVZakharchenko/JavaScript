3834de1c68ef53deac2c129c431877817067f2c4
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-05-21 16:44:00 +0300

    lesson_20_hw_2

---
+++ lesson_20/hw_2/index.js
@@ -0,0 +1,43 @@
+'use strict'
+
+class User {
+    constructor(id, name, sessionId) {
+        this._id = id;
+        this._name = name;
+        this._sessionId = sessionId;
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get sessionId() {
+        return this._sessionId;
+    }
+}
+
+class UserRepository {
+    constructor(users) {
+        this._users = Object.freeze(users);
+    }
+
+    get users() {
+        return this._users;
+    }
+
+    getUserNames() {
+        return this._users.map(item => item._name);
+    }
+
+    getUserIds() {
+        return this.users.map(item => item._id)
+    }
+
+    getUserNameById(id) {
+        return this.users.find(el => el._id === id)._name;
+    }
+}
