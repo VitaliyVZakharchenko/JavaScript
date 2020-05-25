6e527a5e2fdf26f031a91b553e175f9a0f5e2f09
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-05-25 17:20:14 +0300

    lesson_21_hw_3

---
+++ lesson_21/hw_3/index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+export function getTitle() {
+    const title = document.querySelector('.title');
+
+    return title.textContent;
+}
+
+export function getDescription() {
+    const description = document.querySelector('.about');
+
+    return description.innerText;
+}
+
+export function getPlans() {
+    const plans = document.querySelector('.plans');
+
+    return plans.innerHTML;
+}
+
+export function getGoal() {
+    const goal = document.querySelector('.goal');
+
+    return goal.outerHTML;
+}
+
+console.log(getTitle());
+
+console.log(getDescription());
+
+console.log(getPlans());
+
+console.log(getGoal());
