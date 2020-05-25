ea09c0815b637cd8323fb7942fe8d2e3663f7fdc
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-05-25 17:32:04 +0300

    lesson_21_hw_4

---
+++ lesson_21/hw_4/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+export function setTitle(text) {
+    const title = document.querySelector('.title');
+    return title.textContent = text;
+}
+
+// console.log(setTitle('AAdfasfewrfew'));
