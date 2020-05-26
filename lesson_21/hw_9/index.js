e0198355ea222dc27d9f8901d5d18da838e53fd3
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-05-26 17:25:35 +0300

    lesson_21_hw_9

---
+++ lesson_21/hw_9/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const finishForm = () => {
+    const formEl = document.querySelector('.login-form');
+    
+    const newInput = document.createElement('input');
+    
+    const secondInput = document.querySelector('input');
+    secondInput.setAttribute('type', 'password');
+    
+
+    newInput.setAttribute('type', 'text');
+    newInput.setAttribute('name', 'login');
+    formEl.append(newInput);
+}
+
+finishForm();
