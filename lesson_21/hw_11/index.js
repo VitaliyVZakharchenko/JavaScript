7fdddb9a368cd1ee253d9c5d98ec80802d475d92
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-05-28 15:07:48 +0300

    lesson_21_hw_11

--- lesson_21/hw_11/index.js
+++ lesson_21/hw_11/index.js
@@ -1,10 +1,10 @@
 'use strict';
    
 export const squaredNumbers = () => {
-    document.querySelectorAll('.number')
+    Array.from(document.querySelectorAll('.number'))
         .forEach(el => {
             el.dataset.sqNumber = Math.pow(el.dataset.number, 2)
         });
 }
 
-squaredNumbers();
+// squaredNumbers();
