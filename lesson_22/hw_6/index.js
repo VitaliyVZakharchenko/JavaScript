0c454f544552982328ff8fd84640e8ee1e239f36
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-06-02 13:23:22 +0300

    lesson_22_hw_6

---
+++ lesson_22/hw_6/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const btnEl = document.querySelector('.btn');
+const btnEl1 = document.querySelector('.btn').nextElementSibling;
+
+
+const handleClick = () => {
+    console.log(event.target.textContent);
+}
+
+btnEl.addEventListener('click', handleClick);
+btnEl1.addEventListener('click', handleClick);
