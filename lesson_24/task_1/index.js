44ad3549ecb7ace63f6d027b759f26339215635c
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-06-08 16:19:49 +0300

    lesson_24_task_1

---
+++ lesson_24/task_1/index.js
@@ -0,0 +1,12 @@
+const weekDays = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St'];
+
+export const dayOfWeek = (date, days) => {
+    const day = new Date(date).getDate();
+    const dateInFuture = new Date(date).setDate(day + days);
+
+    return weekDays[new Date(dateInFuture).getDay()];
+};
+
+// const result = dayOfWeek(new Date(2019, 0, 1), 8);
+
+// console.log(result);
