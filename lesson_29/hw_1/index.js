f303e382736387afef4915adc99df38f419901e8
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-06-16 15:00:17 +0300

    lesson_29_hw_1

---
+++ lesson_29/hw_1/index.js
@@ -0,0 +1,13 @@
+export const requestUserData = (userId, callback) => {
+    if (userId == 'broken') {
+      callback(null, 'Failed to load user data');
+      return;
+    };
+   
+    const data = {
+      userId: userId,
+      email: `${userId}@example.com`,
+    };
+  
+    setTimeout(() => callback(data), Math.ceil(Math.random() * 3 * 1000));
+};
