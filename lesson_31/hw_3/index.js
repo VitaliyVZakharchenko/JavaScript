0276a667ce11555700deab3ed08622f3b7bb1d02
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-06-18 16:44:26 +0300

    lesson_31

---
+++ lesson_31/hw_3/index.js
@@ -0,0 +1,11 @@
+export const delay = time => {
+    const pr = new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(time)
+    }, time)
+  })
+  return pr
+  }
+  
+  delay(3000)
+  .then(() => console.log('Done'));
