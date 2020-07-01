c96e19bf552f1e8028b16a22c2e19f433f9dbb53
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-07-01 15:02:34 +0300

    lesson_34

--- lesson_34/hw_2/index.js
+++ lesson_34/hw_2/index.js
@@ -2,6 +2,7 @@ const baseUrl = 'https://5efa35d5bc5f8f0016c677c4.mockapi.io/api/v1/users';
 
 const formElem = document.querySelector('.login-form');
 const errorElem = document.querySelector('.error-text');
+const submitBtn = document.querySelector('.submit-button');
 
 const onFormSubmit = event => {
     event.preventDefault();
@@ -11,28 +12,27 @@ const formData = Object.fromEntries(new FormData(formElem));
 fetch(baseUrl, {
     method: 'POST',
     headers: {
-        'Content-Type": "application/json;charset=utf-8',
+        'Content-Type': 'application/json;charset=utf-8'
     },
-    body: JSON.stringify(formData),
+    body: JSON.stringify(formData)
 })
-    .then((response) => response.json())
-        .then((value) => {
-            alert(JSON.stringify(value));
+    .then(response => response.json())
+        .then(response => {
+            alert(JSON.stringify(response));
             formElem.reset();
         })
             .catch(() => {
-            errorElem.textContent = 'Failed to create user';
+            errorElem.textContent = 'Failed to create user'
         });
 };
 
 formElem.addEventListener('submit', onFormSubmit);
-const submitBtn = document.querySelector('.submit-button');
 
 const onInput = () => {
-    errorElem.textContent = null;
-    formElem.reportValidity() 
-    ? (submitBtn.disabled = false)
-    : (submitBtn.disabled = true);
+    errorElem.textContent = '';
+    formElem.reportValidity()
+    ? submitBtn.disabled = false
+    : submitBtn.disabled = true;
 };
 
 formElem.addEventListener('input', onInput);
