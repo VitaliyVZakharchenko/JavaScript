aaac13d59b05e7e20da5aac51810f9b9860de833
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-07-27 11:52:09 +0300

    lesson_36_task_1

--- lesson_36/task_1/index.js
+++ lesson_36/task_1/index.js
@@ -11,6 +11,6 @@ export const fetchUser = async userId => {
     }
 };
 
-getUser('facebook')
+fetchUser('facebook')
     .then(userData => console.log(userData))
     .catch(err => alert(err.message));
