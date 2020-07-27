32e39fdbe6e30c856657c4ace3996548e7642eda
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-07-27 11:50:50 +0300

    lesson_36_task_1

--- lesson_36/task_1/index.js
+++ lesson_36/task_1/index.js
@@ -1,16 +1,13 @@
 export const fetchUser = async userId => {
     try {
         const response = await fetch(`https://api.github.com/users/${userId}`);
-        // console.log(response);
         if (!response.ok) {
             return null;
         }
         const userData = await response.json();
-        // console.log(userData);
         return userData;
     } catch (err) {
         throw new Error('Failed to fetch user');
-        // console.log(err);
     }
 };
 
